70c6b04a2253260030d8b0af5dd3f6387feb370d
Luis Rocha <lerocha@gmail.com>
2017-04-02 01:37:30 -0700

    Getting movie vote average, count and release date

--- Flicks/Movie.swift
+++ Flicks/Movie.swift
@@ -13,6 +13,9 @@ class Movie : NSObject {
     var title: String = ""
     var overview: String = ""
     var posterPath: String = ""
+    var voteAgerage: Double = 0
+    var voteCount: CLongLong = 0
+    var releaseDate: String = ""
     var smallPosterUrl: URL?
     var posterUrl: URL?
     
@@ -24,6 +27,9 @@ class Movie : NSObject {
         title = movie["title"] as! String
         overview = movie["overview"] as! String
         posterPath = movie["poster_path"] as! String
+        voteAgerage = movie["vote_average"] as! Double
+        voteCount = movie["vote_count"] as! CLongLong
+        releaseDate = movie["release_date"] as! String
         smallPosterUrl = URL(string: "https://image.tmdb.org/t/p/w45/\(posterPath)");
         posterUrl = URL(string: "https://image.tmdb.org/t/p/w342/\(posterPath)");
     }
